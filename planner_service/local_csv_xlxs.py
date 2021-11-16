d44d5ca44d7be6a9ec629679fcd3801243d5c408
4ellendger <4ellendger@gmail.com>
2021-11-16 17:00:15 +0300

    Added periods checking, completed template. Works but with strange numbers and slow.

--- planner_service/local_csv_xlxs.py
+++ planner_service/local_csv_xlxs.py
@@ -1,10 +1,14 @@
+from numpy import datetime64
+from numpy.lib.histograms import histogram
 import pandas as pd
 import os
 import sys
 import datetime
 import logging
 import typing
+import enum
 import difflib
+from pandas.core.base import DataError
 import prophet
 
 
@@ -111,12 +115,28 @@ def read_ets_dumps_and_merge(folder_path: str) -> pd.DataFrame:
     logging.debug(data.describe)
     return data
 
+
+class Period(enum.Enum):
+    DAILY = 1
+    WEEKLY = 7
+    MONTHLY = 30
+    YEARLY = 365
+
+
 class Tokenizer:
 
     def __init__(self, df:pd.DataFrame):
         self.df = df
 
-    def get_unit_histories(self) -> typing.Dict[typing.Any, pd.DataFrame]:
+    def get_unit_histories(self, period: Period) -> typing.Dict[typing.Any, pd.DataFrame]:
+        """
+        Does main logic of class - finds all relevant units (rows to predict) in dataframe and separates dataframe
+        per each unit.
+        Unit here may be any hashable type (because may span few columns) and it is tokenizer-specific type.
+        :param period: Period for which need to find relevant units. For example it is useless to provide few years
+        history for "next day" prediciton.
+        :return: Dictionary with dataframe (histories) per found unit.
+        """
         raise NotImplemented()
 
     def expand_unit_prediction(self, unit: typing.Any, y: float) -> dict:
@@ -129,6 +149,20 @@ class Tokenizer:
         for unit, df in unit_histories.items():
             df.to_csv(self.unit_to_str(unit) + ".csv", header=True)
 
+    def _limit_history_by_period(self, df: pd.DataFrame, last_day_in_data: datetime64, period: Period) -> pd.DataFrame:
+        # Measure relative to last day in the whole history data, not to "day to predict"!
+        # 1. Last occurence shouldn't be older than 30 periods.
+        # 2. Max history is 60 periods.
+        # 3. At least one row in result.
+        if df is None or df.empty:
+            return None
+        last_day = df['ds'].iloc[-1]
+        if (last_day_in_data - last_day).days > 30 * period.value:
+            return None
+        first_day = last_day_in_data - pd.to_timedelta(60 * period.value, unit="d")
+        df = df.drop(df[df['ds'] < first_day].index)
+        return None if df.empty else df
+
     def _separate_by_task(self):
         tasks = self.df['task'].unique()
         return dict((x, self.df[self.df['task'] == x]) for x in tasks)
@@ -137,20 +171,25 @@ class Tokenizer:
 class SameDescriptionTokenizer(Tokenizer):
     """
     Simple tokens matcher with "same task and description".
+    Removes tokens not appearing long before.
     2020 Jan+Dec AM dataset - only 5 tokens with > 4 items.
     """
 
     def __init__(self, df: pd.DataFrame):
         super().__init__(df)
 
-    def get_unit_histories(self) -> typing.Dict[str, pd.DataFrame]:
+    def get_unit_histories(self, period: Period) -> typing.Dict[str, pd.DataFrame]:
         result = dict()
+        last_day_in_data = self.df['ds'].iloc[-1]
         for task, df in self._separate_by_task().items():
             for desc in df['description'].unique():
-                result[(task, desc)] = df[df['description'] == desc]
+                history = df[df['description'] == desc]
+                history = self._limit_history_by_period(history, last_day_in_data, period)
+                if history is not None:
+                    result[(task, desc)] = history
         return result
 
-    def expand_unit_prediction(self, unit, y: float) -> dict:
+    def expand_unit_prediction(self, unit: typing.Any, y: float) -> dict:
         return {
             'task': unit[0],
             'effort': y,
@@ -181,11 +220,14 @@ def timeit(name:str, func, *args) -> typing.Any:
     start_time = datetime.datetime.now()
     result = func(*args)
     logging.debug(f"{name} took {datetime.datetime.now() - start_time}.")
+    return result
 
 
 def run_prophet(df: pd.DataFrame, date: str) -> float:
-    m = prophet.Prophet().fit(df)
-    pass  # See jupiter "ets"
+    model = prophet.Prophet().fit(df)
+    future = pd.DataFrame((pd.to_datetime(date),), columns=['ds'])
+    forecast = model.predict(future)
+    return forecast.at[0, 'yhat']  # 0 index because only one row was asked.
 
 
 def predict_unit(data: pd.DataFrame, date: str, y_col:str, threshold: float) -> typing.Optional[pd.Index]:
@@ -200,8 +242,21 @@ def predict_unit(data: pd.DataFrame, date: str, y_col:str, threshold: float) -> 
         return None
     # Predict real value for this day.
     df = data.loc[:, ['ds', y_col]]
-    df.rename(axis=1, columns={y_col: 'y'})
+    df.rename(columns={y_col: 'y'}, inplace=True)
     return run_prophet(df, date)
+
+
+def predict_day(unit_histories: typing.Dict[typing.Any, pd.DataFrame], date: str, threshold_to_take_unit: float)\
+        -> typing.List[dict]:
+    day_prediction = []
+    for unit, unit_data in unit_histories.items():
+        y = timeit(f"Predict {tokenizer.unit_to_str(unit)}", predict_unit,
+                    unit_data, date, 'effort', threshold_to_take_unit)
+        if y is not None:
+            row = tokenizer.expand_unit_prediction(unit, y)
+            row['date'] = date
+            day_prediction.append(row)
+    return day_prediction
 
 
 if __name__ == "__main__":
@@ -212,6 +267,7 @@ if __name__ == "__main__":
         data.to_csv(DUMP_FILEPATH, index=False)
     else:
         data = pd.read_csv(DUMP_FILEPATH, header=0)
+        data['ds'] = pd.DatetimeIndex(data['ds'])
     print(data.head)
     # Idea to predict ETS is follow:
     # 1) Divide rows on 'predict units' by project and description (by complexity stages)
@@ -222,20 +278,14 @@ if __name__ == "__main__":
     # 2) Predict probability of each 'predict unit' on new day. Remove units by some threshold.
     # 3) Predict effort of selected units on this day.
     dates_to_predict = ['2021-01-06', '2021-01-08', '2021-01-11', '2021-01-12', '2021-01-13']
+    period = Period.DAILY
     threshold_to_take_unit = 0.8
     tokenizer = SameDescriptionTokenizer(data)
     prediction = pd.DataFrame()
-    unit_histories = timeit(f"Tokenize {len(data)} rows",  tokenizer.get_unit_histories)
+    unit_histories = timeit(f"Tokenize {len(data)} rows",  tokenizer.get_unit_histories, period)
     logging.info(f"From {len(data)} rows got {len(unit_histories)} units")
     for date in dates_to_predict:
-        day_prediction = []
-        for unit, unit_data in unit_histories.items():
-            y = timeit(f"Predict {tokenizer.unit_to_str(unit)}", predict_unit,
-                       unit_data, date, 'effort', threshold_to_take_unit)
-            if y is not None:
-                row = tokenizer.expand_unit_prediction(unit, y)
-                row['date'] = date
-                day_prediction.append(row)
+        day_prediction = timeit(f"Predict {date}", predict_day, unit_histories, date, threshold_to_take_unit)
         logging.debug(f"{date}: predicted {len(day_prediction)} rows")
         prediction.append(day_prediction)
     logging.info(prediction.describe)
